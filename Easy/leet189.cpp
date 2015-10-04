88ce7f292c75fdb4383aef96e43c8ddbc9785ebe
xpharry <xpharry123@hotmail.com>
2015-10-04 01:15:46 -0400

    beginning exercising leetcode

---
+++ Easy/leet189.cpp
@@ -0,0 +1,1 @@
+class Solution {public:    void rotate(vector<int>& nums, int k) {    	for (int i = 0; i < k; ++i) {    		nums.insert(nums.begin(),nums[nums.end()-1]);    		nums.pop_back();    	}            }};
