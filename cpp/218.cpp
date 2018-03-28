1830c1e4407b7dd31d8c6c4081671036ecba17ad
xpharry <robotpengxu@gmail.com>
2018-03-28 18:57:58 -0400

    218

--- cpp/218.cpp
+++ cpp/218.cpp
@@ -1,30 +1,34 @@
+/**
+ *
+ */
 class Solution {
 public:
     vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) {
-        vector<pair<int, int>> res;
-        int cur=0, cur_X, cur_H =-1,  len = buildings.size();
-        priority_queue< pair<int, int>> liveBlg; // first: height, second, end time
-        while(cur<len || !liveBlg.empty())
-        { // if either some new building is not processed or live building queue is not empty
-            cur_X = liveBlg.empty()? buildings[cur][0]:liveBlg.top().second; // next timing point to process
-
-            if(cur>=len || buildings[cur][0] > cur_X)
-            { //first check if the current tallest building will end before the next timing point
-                  // pop up the processed buildings, i.e. those  have height no larger than cur_H and end before the top one
-                while(!liveBlg.empty() && ( liveBlg.top().second <= cur_X) ) liveBlg.pop();
+        vector<pair<int, int>> h, res;
+        multiset<int> m;
+        int pre = 0, cur = 0;
+        for (auto &a : buildings) {
+            h.push_back({a[0], -a[2]});
+            h.push_back({a[1], a[2]});
+        }
+        sort(h.begin(), h.end());
+        m.insert(0);
+        for (auto &a : h) {
+            if (a.second < 0) m.insert(-a.second);
+            else m.erase(m.find(a.second));
+            cur = *m.rbegin();
+            if (cur != pre) {
+                res.push_back({a.first, cur});
+                pre = cur;
             }
-            else
-            { // if the next new building starts before the top one ends, process the new building in the vector
-                cur_X = buildings[cur][0];
-                while(cur<len && buildings[cur][0]== cur_X)  // go through all the new buildings that starts at the same point
-                {  // just push them in the queue
-                    liveBlg.push(make_pair(buildings[cur][2], buildings[cur][1]));
-                    cur++;
-                }
-            }
-            cur_H = liveBlg.empty()?0:liveBlg.top().first; // outut the top one
-            if(res.empty() || (res.back().second != cur_H) ) res.push_back(make_pair(cur_X, cur_H));
         }
         return res;
     }
 };
+
+
+/**
+ * Geometry, Sweep Line
+ *
+ * Ref: huahua
+ */
