79d344ebc5f9faa85ebb3fdc60a5f2ad58ee260e
Peng Xu <xpharry@users.noreply.github.com>
2018-06-09 21:16:30 -0400

    Update 144-BinaryTreePreorderTraversal.cpp

--- cpp/144-BinaryTreePreorderTraversal.cpp
+++ cpp/144-BinaryTreePreorderTraversal.cpp
@@ -1,10 +1,25 @@
 /*****
+ * Solution 1:
  * Binary Tree, Recursion
  *
  */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(!root) return res;
+        res.push_back(root->val);
+        if(root->left) preorderTraversal(root->left);
+        if(root->right) preorderTraversal(root->right);
+        return res;
+    }
+    
+private:
+    vector<int> res;
+};
 
 
 /*****
+ * Solution 2:
  * Binary Tree, Iteration
  *
  */
@@ -23,6 +38,33 @@ public:
             if(p->left) s.push(p->left);
         }
         
+        return res;
+    }
+};
+
+
+
+/*****
+ * Solution 3:
+ * Binary Tree
+ *
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> s;
+        TreeNode *p = root;
+        while (!s.empty() || p) {
+            if (p) {
+                s.push(p);
+                res.push_back(p->val);
+                p = p->left;
+            } else {
+                TreeNode *t = s.top(); s.pop();
+                p = t->right;
+            }
+        }
         return res;
     }
 };
