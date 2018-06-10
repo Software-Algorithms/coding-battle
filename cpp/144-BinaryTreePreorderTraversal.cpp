767f4032b3f821367d745db6e7b7727240317e09
Peng Xu <xpharry@users.noreply.github.com>
2018-06-09 20:31:40 -0400

    Update 144-BinaryTreePreorderTraversal.cpp

--- cpp/144-BinaryTreePreorderTraversal.cpp
+++ cpp/144-BinaryTreePreorderTraversal.cpp
@@ -1,11 +1,12 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
+/*****
+ * Binary Tree, Recursion
+ *
+ */
+
+
+/*****
+ * Binary Tree, Iteration
+ *
  */
 class Solution {
 public:
