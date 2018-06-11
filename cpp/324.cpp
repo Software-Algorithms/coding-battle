fa85c12b3776cc8e8fad0a273547a2ccd1dc910f
Peng Xu <xpharry@users.noreply.github.com>
2018-06-11 16:22:37 -0400

    Update 324.cpp

--- cpp/324.cpp
+++ cpp/324.cpp
@@ -25,14 +25,21 @@ public:
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        vector<int> tmp = nums;
-        int n = nums.size(), k = (n + 1) / 2, j = n; 
-        sort(tmp.begin(), tmp.end());
-        for (int i = 0; i < n; ++i) {
-            nums[i] = i & 1 ? tmp[--j] : tmp[--k];
+        #define A(i) nums[(1 + 2 * i) % (n | 1)]
+        int n = nums.size(), i = 0, j = 0, k = n - 1;
+        auto midptr = nums.begin() + n / 2;
+        nth_element(nums.begin(), midptr, nums.end());
+        int mid = *midptr;
+        while (j <= k) {
+            if (A(j) > mid) swap(A(i++), A(j++));
+            else if (A(j) < mid) swap(A(j), A(k--));
+            else ++j;
         }
     }
 };
 
 // Conclusion:
-// O(n) space
+// O(1) space
+
+
+
