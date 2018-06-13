21a7bbda447650eefa10448f0208e4ebdabec0db
Peng Xu <xpharry@users.noreply.github.com>
2018-06-12 22:39:33 -0400

    Update 303.cpp

--- cpp/303.cpp
+++ cpp/303.cpp
@@ -2,6 +2,8 @@
  * DP
  *
  */
+
+//version 1:
 class NumArray {
 public:
     NumArray(vector<int> nums) {
@@ -19,6 +21,24 @@ private:
     vector<int> dp;
 };
 
+// version 2:
+class NumArray {
+public:
+    NumArray(vector<int> &nums) {
+        dp.resize(nums.size() + 1, 0);
+        for (int i = 1; i <= nums.size(); ++i) {
+            dp[i] = dp[i - 1] + nums[i - 1];
+        }
+    }
+    int sumRange(int i, int j) {
+        return dp[j + 1] - dp[i];
+    }
+    
+private:
+    vector<int> dp;
+};
+
 // Conclusion:
 // Decrease the time complexity of the method that would be called frequntly.
+// In version 2, the add-on is to avoid zero check.
 
