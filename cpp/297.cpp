14a0ddc42aa5b1e1a27c1c1470d21f14768f406d
Peng Xu <xpharry@users.noreply.github.com>
2018-06-14 02:21:35 -0400

    Create 297.cpp

---
+++ cpp/297.cpp
@@ -0,0 +1,57 @@
+/*
+ * Tree, Design
+ *
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        ostringstream out;
+        serialize(root, out);
+        return out.str();
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        istringstream in(data);
+        return deserialize(in);
+    }
+    
+private:
+    void serialize(TreeNode *root, ostringstream &out) {
+        if (root) {
+            out << root->val << ' ';
+            serialize(root->left, out);
+            serialize(root->right, out);
+        } else {
+            out << "# ";
+        }
+    }
+    
+    TreeNode* deserialize(istringstream &in) {
+        string val;
+        in >> val;
+        if (val == "#") return nullptr;
+        TreeNode *root = new TreeNode(stoi(val));
+        root->left = deserialize(in);
+        root->right = deserialize(in);
+        return root;
+    }
+};
+
+// Conclusion:
+//
+// istringstream, ostringstream
+//
+// istringstream -> string
+// in >> str;
+// 
+// ostringstream -> string
+// str = out.str();
+//
+// string -> ostringstream
+// out << str;
+
+
+
