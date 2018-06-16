1538a879dbf3d3531a5c469433669e8b055c76b1
Peng Xu <xpharry@users.noreply.github.com>
2018-06-16 16:37:22 -0400

    Update 847.cpp

--- cpp/847.cpp
+++ cpp/847.cpp
@@ -5,29 +5,35 @@
 class Solution {
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
-        const int n = graph.size();
-        const int kAns = (1 << n) - 1;
+        int n = graph.size();
+        int goal = (1 << n) - 1;
+        
         queue<pair<int, int>> q;
-        vector<vector<int>> visited(n, vector<int>(1 << n));
-        for (int i = 0; i < n; ++i)
-          q.push({i, 1 << i});
-        int steps = 0;
-        while (!q.empty()) {
-          int s = q.size();
-          while (s--) {
-            auto p = q.front(); 
-            q.pop();
-            int node = p.first;
-            int state = p.second;
-            if (state == kAns) return steps;   
-            if (visited[node][state]) continue;
-            visited[node][state] = 1;
-            for (int next : graph[node])
-              q.push({next, state | (1 << next)});
-          }
-          ++steps;
+        for(int i = 0; i < n; i++) {
+            q.push({i, 1 << i});
         }
-        return -1;        
+        
+        vector<vector<int>> visited(n, vector<int>(1 << n));
+        
+        int steps = 0;
+        while(!q.empty()) {
+            int sz = q.size();
+            for(int i = 0; i < sz; i++) {
+                auto t = q.front();
+                q.pop();
+                int node = t.first;
+                int state = t.second;
+                if(state == goal) return steps;
+                if(visited[node][state]) continue;
+                visited[node][state] = 1;
+                for(int next : graph[node]) {
+                  q.push({next, state | (1 << next)});
+                }               
+            }
+            steps++;
+        }
+        
+        return -1;
     }
 };
 
