a854cc16c8f65d0a00865a9897106ca2dd5413aa
Peng Xu <xpharry@users.noreply.github.com>
2018-06-16 15:54:45 -0400

    Update 271.cpp

--- cpp/271.cpp
+++ cpp/271.cpp
@@ -2,6 +2,38 @@
  * String, Design
  *
  */
+
+// version 1:
+class Codec {
+public:
+
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs) {
+        string encoded;
+        for(const auto &s : strs) {
+            int len = s.size();
+            encoded += to_string(len) + '/' + s;
+        }
+        return encoded;
+    }
+
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s) {
+        vector<string> decoded;
+        int i = 0, n = s.size();
+        while(i < n) {
+            int start = i;
+            while(i < n && s[i] != '/') i++; // find the position of '/'
+            int len = stoi(s.substr(start, i-start));
+            start = ++i;
+            decoded.push_back(s.substr(start, len));
+            i += len;
+        }
+        return decoded;
+    }
+};
+
+// version 2:
 class Codec {
 public:
 
