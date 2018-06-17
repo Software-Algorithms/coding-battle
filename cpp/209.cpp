42047a693498f26ed7d90ce12d039f87d3a35a60
Peng Xu <xpharry@users.noreply.github.com>
2018-06-17 15:56:00 -0400

    Update 209.cpp

--- cpp/209.cpp
+++ cpp/209.cpp
@@ -1,6 +1,9 @@
-/**
+/*
  * Two Pointers
+ * 
  */
+
+// version 1: i as end, start as start
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
@@ -15,3 +18,28 @@ public:
         return minlen == INT_MAX ? 0 : minlen;
     }
 };
+
+// version 2: i as start, j as end
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {    
+        int i = 0, j = 0;
+        int sum = 0, min_len = INT_MAX;
+        for(; i < nums.size(); i++) {
+            while(j < nums.size() && sum < s) {
+                sum += nums[j];
+                j++;
+            }
+            if(sum >= s) min_len = min(min_len, j - i);
+            sum -= nums[i];
+        }
+        return min_len == INT_MAX ? 0 : min_len;
+    }
+};
+
+// Conclusion:
+// Can use a generic template to solve this kind of problems.
+//
+
+
+
