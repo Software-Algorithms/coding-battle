a4ae94e6ff3fd969105494a0aa2caf4f0b33501b
Peng Xu <xpharry@users.noreply.github.com>
2018-06-17 16:22:30 -0400

    Update 003-LongestSubstringWithoutRepeatingCharacters.cpp

--- cpp/003-LongestSubstringWithoutRepeatingCharacters.cpp
+++ cpp/003-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,3 +1,9 @@
+/*
+ * Two Pointers
+ *
+ */
+
+// version 1:
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -17,3 +23,31 @@ public:
         return res;
     }
 };
+
+// version 2:
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int max_len = 0;
+        int i = 0, j = 0;
+        
+        unordered_map<char, int> m;
+        
+        while(i <= j && j < s.size()) {
+            if(!m.count(s[j])) {
+                m[s[j++]] = 1;
+            } else {
+                m.erase(s[i++]);
+            }
+            max_len = max(max_len, j-i);
+        }
+        
+        return max_len;
+    }
+};
+
+// Conclusion:
+//
+
+
+
