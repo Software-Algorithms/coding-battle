d8f47f573f47e67810937ccfaccd50bc36d6c665
Peng Xu <xpharry@users.noreply.github.com>
2018-06-17 01:39:33 -0400

    Create 317.cpp

---
+++ cpp/317.cpp
@@ -0,0 +1,59 @@
+/*
+ * BFS
+ *
+ */
+class Solution {
+public:
+    int shortestDistance(vector<vector<int>>& grid) {
+        int res = INT_MAX;
+        int buildingCnt = 0;
+        int m = grid.size(), n = grid[0].size();
+        
+        vector<vector<int>> dist(m, vector<int>(n, 0));
+        vector<vector<int>> cnt(m, vector<int>(n, 0));
+        vector<vector<int>> dirs{{0,-1},{-1,0},{0,1},{1,0}};
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) { // BFS
+                    ++buildingCnt;
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    vector<vector<bool>> visited(m, vector<bool>(n, false));
+                    int level = 1;
+                    while (!q.empty()) {
+                        int size = q.size();
+                        for (int s = 0; s < size; ++s) {
+                            int a = q.front().first, b = q.front().second; q.pop();
+                            for (int k = 0; k < dirs.size(); ++k) {
+                                int x = a + dirs[k][0], y = b + dirs[k][1];
+                                if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 0 && !visited[x][y]) {
+                                    dist[x][y] += level;
+                                    ++cnt[x][y];
+                                    visited[x][y] = true;
+                                    q.push({x, y});
+                                }
+                            }
+                        }
+                        ++level;
+                    }
+                }
+            }
+        }
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 0 && cnt[i][j] == buildingCnt) {
+                    res = min(res, dist[i][j]);
+                }
+            }
+        }
+        
+        return res == INT_MAX ? -1 : res;        
+    }
+};
+
+// Conclusion:
+
+
+
