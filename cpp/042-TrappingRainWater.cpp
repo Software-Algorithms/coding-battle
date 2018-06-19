1a4f218896b91adb905d50b65b8e948fcf702386
Peng Xu <xpharry@users.noreply.github.com>
2018-06-19 16:28:31 -0400

    Update 042-TrappingRainWater.cpp

--- cpp/042-TrappingRainWater.cpp
+++ cpp/042-TrappingRainWater.cpp
@@ -1,28 +1,86 @@
+/*
+ * Solution 1:
+ * DP
+ * Two rounds.
+ */
 class Solution {
 public:
     int trap(vector<int>& height) {
-        if(height.size()<=2) return 0;
-        int l = 0;
-        int r = height.size()-1;
-        int maxLf = height[l], maxRh = height[r];
-        int res = 0;
-        while(l<r) {
-            if(maxLf < maxRh) {
-                l++;
-                if(maxLf < height[l]) {
-                    maxLf = height[l];
-                } else {
-                    res += maxLf - height[l];
-                }           
+        int res = 0, mx = 0, n = height.size();
+        vector<int> dp(n, 0);
+        for (int i = 0; i < n; ++i) {
+            dp[i] = mx;
+            mx = max(mx, height[i]);
+        }
+        mx = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i] = min(dp[i], mx);
+            mx = max(mx, height[i]);
+            if (dp[i] > height[i]) res += dp[i] - height[i];
+        }
+        return res;
+    }
+};
+
+
+/*
+ * Solution 2:
+ * DP + Two PointerS
+ * ONE PASS
+ *
+ */
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int res = 0, l = 0, r = height.size() - 1;
+        while (l < r) {
+            int mn = min(height[l], height[r]);
+            if (mn == height[l]) {
+                ++l;
+                while (l < r && height[l] < mn) {
+                    res += mn - height[l++];
+                }
             } else {
-                r--;
-                if(maxRh < height[r]) {
-                    maxRh = height[r];
-                } else {
-                    res += maxRh - height[r];
-                }             
+                --r;
+                while (l < r && height[r] < mn) {
+                    res += mn - height[r--];
+                }
             }
         }
         return res;
     }
 };
+
+// Conclusion:
+// 
+
+
+/*
+ * Solution 3:
+ * Stack
+ *
+ */
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        stack<int> st;
+        int i = 0, res = 0, n = height.size();
+        while (i < n) {
+            if (st.empty() || height[i] <= height[st.top()]) {
+                st.push(i++);
+            } else {
+                int t = st.top(); st.pop();
+                if (st.empty()) continue;
+                res += (min(height[i], height[st.top()]) - height[t]) * (i - st.top() - 1);
+            }
+        }
+        return res;
+    }
+};
+
+// Conclusion:
+
+
+
+
+
