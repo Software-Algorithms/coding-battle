3efe0104f37e716f184729fd5c6573c97ec16ec4
Peng Xu <xpharry@users.noreply.github.com>
2018-06-19 18:09:33 -0400

    Create 295.cpp

---
+++ cpp/295.cpp
@@ -0,0 +1,55 @@
+/*
+ * Heap (priority_queue), Design
+ *
+ */
+class MedianFinder {
+public:
+    /** initialize your data structure here. */
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        small.insert(num);
+        large.insert(-*small.begin());
+        small.erase(small.begin());
+        if(small.size() < large.size()) {
+            small.insert(-*large.begin());
+            large.erase(large.begin());
+        }
+    }
+    
+    double findMedian() {
+        return small.size() > large.size() ? *small.begin() : 0.5 * (*small.begin() - *large.begin());
+    }
+    
+private:
+    multiset<int> small, large;
+};
+
+
+/*
+ * Heap (multi_set: binary search tree), Design
+ *
+ */
+ 
+// Conclusion:
+// Maintain a maxheap and a min heap. The median is only related to the top of the two heaps.
+//
+// Implement Heap with priority queue or multiset.
+// priority_queue<T>
+//
+// multiset<T>
+// Multisets are typically implemented as binary search trees.
+// Difference with set<T>: Set and multiset containers sort their elements automatically according
+//                         to a certain sorting criterion. The difference between the two is that 
+//                         multisets allow duplicates, whereas sets do not.
+//                         To use a set or multiset, you must include the header file <set>
+// If a special sorting criterion is not passed, the default criterion less is used. The function
+// object less sorts the elements by comparing them with operator <.
+//
+// Reference:
+// Grandyang: http://www.cnblogs.com/grandyang/p/4896673.html
+
+
+
