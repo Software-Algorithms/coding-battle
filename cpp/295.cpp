031ea215fe44f43afe0adf752d64b9f736fbde3c
Peng Xu <xpharry@users.noreply.github.com>
2018-06-19 22:16:05 -0400

    Update 295.cpp

--- cpp/295.cpp
+++ cpp/295.cpp
@@ -4,26 +4,39 @@
  */
 class MedianFinder {
 public:
-
-    // Adds a number into the data structure.
+    /** initialize your data structure here. */
+    MedianFinder() {
+        
+    }
+    
     void addNum(int num) {
         small.push(num);
         large.push(-small.top());
         small.pop();
-        if (small.size() < large.size()) {
+        if(large.size() > small.size()) {
             small.push(-large.top());
             large.pop();
         }
     }
-
-    // Returns the median of current data stream
+    
     double findMedian() {
-        return small.size() > large.size() ? small.top() : 0.5 *(small.top() - large.top());
+        if(small.size() > large.size()) {
+            return small.top();
+        } else {
+            return ( small.top() - large.top() ) / 2;
+        }
     }
-
+    
 private:
-    priority_queue<long> small, large;
+    priority_queue<double> small, large; // descending order, top is largest
 };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
 
 
 /*
